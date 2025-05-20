69801b97da4be634f2fdae0234717c2879f0d9c4
Madhan D <154608700+Madhan-D249@users.noreply.github.com>
2025-05-20 17:12:28 +0530

    Update main.tf

--- three_tier_architech/main.tf
+++ three_tier_architech/main.tf
@@ -1,27 +1,31 @@
+terraform {
+  required_providers {
+    aws = {
+       version = ">= 5.0.0"
+    }
+  }
+}
 provider "aws" {
   region     = var.region
-  access_key = "*************"
-  secret_key = "***************************"
+  access_key = "***************"
+  secret_key = "***********************"
 }
-
-resource "aws_vpc" "main" {
+resource "aws_vpc" "maxvpc" {
   cidr_block           = "10.0.0.0/24"
   enable_dns_support   = true
   enable_dns_hostnames = true
   tags = {
-    Name = "main-vpc"
+    Name = "my-vpc"
   }
 }
-
 resource "aws_internet_gateway" "igw" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.maxvpc.id
   tags = {
     Name = "main-igw"
   }
 }
-
 resource "aws_subnet" "public" {
-  vpc_id                  = aws_vpc.main.id
+  vpc_id                  = aws_vpc.maxvpc.id
   cidr_block              = "10.0.0.0/25"
   map_public_ip_on_launch = true
   availability_zone       = "ap-south-1a"
@@ -29,23 +33,20 @@ resource "aws_subnet" "public" {
     Name = "public-subnet"
   }
 }
-
 resource "aws_subnet" "private" {
-  vpc_id            = aws_vpc.main.id
+  vpc_id            = aws_vpc.maxvpc.id
   cidr_block        = "10.0.0.128/25"
   availability_zone = "ap-south-1a"
   tags = {
     Name = "private-subnet"
   }
 }
-
 resource "aws_eip" "nat" {
   domain = "vpc"
   tags = {
     Name = "nat-eip"
   }
 }
-
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat.id
   subnet_id     = aws_subnet.public.id
@@ -53,43 +54,36 @@ resource "aws_nat_gateway" "nat" {
     Name = "nat-gateway"
   }
 }
-
 resource "aws_route_table" "public" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.maxvpc.id
   tags = {
     Name = "public-rtb"
   }
 }
-
 resource "aws_route" "public_internet_access" {
   route_table_id         = aws_route_table.public.id
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
-
 resource "aws_route_table_association" "public_subnet" {
   subnet_id      = aws_subnet.public.id
   route_table_id = aws_route_table.public.id
 }
-
 resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+  vpc_id = aws_vpc.maxvpc.id
   tags = {
     Name = "private-rtb"
   }
 }
-
 resource "aws_route" "private_nat_access" {
   route_table_id         = aws_route_table.private.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = aws_nat_gateway.nat.id
 }
-
 resource "aws_route_table_association" "private_subnet" {
   subnet_id      = aws_subnet.private.id
   route_table_id = aws_route_table.private.id
 }
-
 resource "aws_instance" "app_server" {
   ami           = var.ami_id # Replace with a valid AMI ID
   instance_type = var.instance_type
@@ -100,20 +94,3 @@ resource "aws_instance" "app_server" {
   }
 }
 
-resource "aws_instance" "db_server" {
-  ami           = var.ami_id # Replace with a valid AMI ID
-  instance_type = var.instance_type
-  key_name      = var.key_name
-  subnet_id     = aws_subnet.private.id
-  tags = {
-    Name = "db-server"
-  }
-}
-#cre_time
-#terraform {
-  #required_providers {
-    #aws = {
-    #   version = ">= 5.0.0"
-  #  }
- # }
-#}
