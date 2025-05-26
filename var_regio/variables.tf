726212da7b5f81facbd4fc67ca243bcd150922b5
Madhan D <154608700+Madhan-D249@users.noreply.github.com>
2025-05-26 20:18:31 +0530

    Update variables.tf

--- var_regio/variables.tf
+++ var_regio/variables.tf
@@ -1,16 +1,31 @@
-variable "region"{
-    description ="AWS region for the deployment"
-    type=string
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1" # Change to your desired region
 }
-variable "ami_id"{
-    description ="ami id for ec2 instance"
-    type=string
+
+variable "access_key" {
+  description = "AWS access key"
+  type        = string
 }
-variable "instance_type"{
-    description ="ec2-instance type"
-    type=string
+
+variable "secret_key" {
+  description = "AWS secret key"
+  type        = string
 }
-variable "key_name"{
-    description ="key pair for instance"
-    type=string
+
+variable "ami_id" {
+  description = "AMI ID to use for the instances"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+  default     = "t2.medium"
+}
+
+variable "key_name" {
+  description = "Name of the key pair"
+  type        = string
 }
