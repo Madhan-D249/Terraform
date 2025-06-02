d8cc4c2659312d44eaa2158e2b9199d081862b92
Madhan D <154608700+Madhan-D249@users.noreply.github.com>
2025-06-02 15:18:58 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_security_group" "example_sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]0
+    cidr_blocks = ["0.0.0.0/0"]
   }
   egress {
     from_port   = 0
